6def56ffea06017b28e946d18aa1395e9b88630d
Aanya Krishnan <aanyask5@gmail.com>
2023-08-02 12:25:56 -0500

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Aanya on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
